ebc25458cbc2aa5299050e81bc29ccbebf27aaf6
Logesh-Kumar07 <24bcs130@kgcas.com>
2025-10-04 09:39:26 +0530

    program6

---
+++ program6/script6.js
@@ -0,0 +1,27 @@
+  function myfunc(){
+
+        const taskText =document.getElementById("input").value.trim();
+        if(taskText === "") return ;
+        const li = document.createElement("li");
+        li.textContent = taskText;
+
+        li.addEventListener("click",function() {
+        li.classList.toggle("completed");
+        });
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        removeBtn.className = "remove";
+
+        removeBtn.addEventListener("click", function(e) {
+        e.stopPropagation();
+        li.remove();
+        }
+    );
+
+    
+                li.appendChild(removeBtn);
+        document.getElementById("output").appendChild(li);
+        document.getElementById("input").value = "";
+
+    } 
